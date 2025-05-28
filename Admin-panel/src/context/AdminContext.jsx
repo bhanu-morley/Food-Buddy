754880dbaeafa1bd63ff3a3cef35b524936084e6
bhanu-morley <reganabhanuprasanth333@gmail.com>
2025-05-28 12:28:38 +0530

    third-commit

--- Admin-panel/src/context/AdminContext.jsx
+++ Admin-panel/src/context/AdminContext.jsx
@@ -4,7 +4,7 @@ export const AdminContext = createContext();
 
 const AdminContextProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('adminToken') || '');
-  const url = 'http://localhost:4000';
+  const url = 'https://back-end-p345.onrender.com';
 
   useEffect(() => {
     localStorage.setItem('adminToken', token);
